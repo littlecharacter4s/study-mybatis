49b8993ab8b54e0d149a87ac5f4fb69f5ebab807
littlecharacter <littlecharacter@qq.com>
2020-10-20 21:16:24 +0800

    增加原生JDBC示例

--- src/test/java/com/lc/mybatis/JdbcTest.java
+++ src/test/java/com/lc/mybatis/JdbcTest.java
@@ -18,6 +18,7 @@ public class JdbcTest {
         Class.forName(driver);
         // 2.获取数据库连接
         Connection connection = DriverManager.getConnection(url, username, password);
+        // connection.setAutoCommit(false);
         String sql = "select * from user where id = ? ";
         // 3.创建Statement对象（每一个Statement为一次数据库执行请求）
         PreparedStatement stmt = connection.prepareStatement(sql);
@@ -35,6 +36,7 @@ public class JdbcTest {
             }
             System.out.println(map);
         }
+        // connection.commit();
         // 7.各种关闭
         if (rs != null) {
             rs.close();
