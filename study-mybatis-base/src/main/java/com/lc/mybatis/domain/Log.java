3b01b9e51fff66996511c858995852220314533f
gujixian <littlecharacter@qq.com>
2022-05-03 16:47:51 +0800

    重构Study-MyBatis

---
+++ study-mybatis-base/src/main/java/com/lc/mybatis/domain/Log.java
@@ -0,0 +1,55 @@
+package com.lc.mybatis.domain;
+
+import java.util.Date;
+
+public class Log {
+    private Long id;
+
+    private String action;
+
+    private String message;
+
+    private Long operatorId;
+
+    private Date createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Long operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
