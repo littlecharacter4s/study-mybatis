f88db6baf8bbb7dc9ceeba643ac04a9dbf8daa99
gujixian <littlecharacter@qq.com>
2022-05-03 17:41:26 +0800

    重构Study-MyBatis spring-boot hello

---
+++ study-mybatis-plus/src/main/java/com/lc/mybatis/plus/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.lc.mybatis.plus.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello() {
+        return "Hello World";
+	}
+}
